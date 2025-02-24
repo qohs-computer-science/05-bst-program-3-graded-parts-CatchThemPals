ffb31264ac6418a8a89ef7e7a8847c2ec2c57efa
CatchThemPals <sx86851@gmail.com>
2025-02-23 21:42:08 -0500

    Update BST.java

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -6,13 +6,62 @@ public BST()
   {
     TreeNode(null)
   }
+  public void printPreOrder()
+  {
+    if(root != null)
+    {
+      system.out.println(root.getValue());
+      printPreHelper(root.getLeft())
+      printPreHelper(root.getRight())
+    }
+  }
+
+Private void printPreHelper(TN parent)
+  {
+    if(subroot != null)
+    {
+      printPreHelper(parent.getLeft())
+      system.out.println(parent.getValue());
+      printPreHelper(parent.getRight()
+    }
+  }
 public void printInOrder()
   {
     if(root != null)
     {
-    printInHelper(root.getLeft());
+      printInHelper(root.getLeft())
+      system.out.println(root.getValue());
+      printInHelper(root.getRight())
     }
   }
 
-Private void printInHelper(TN subroot)
+Private void printInHelper(TN parent)
+  {
+    if(subroot != null)
+    {
+      printInHelper(parent.getLeft())
+      system.out.println(parent.getValue());
+      printInHelper(parent.getRight()
+    }
+  }
+
+public void printInOrder()
+  {
+    if(root != null)
+    {
+      printInHelper(root.getLeft())
+      system.out.println(root.getValue());
+      printInHelper(root.getRight())
+    }
+  }
+
+Private void printInHelper(TN parent)
+  {
+    if(subroot != null)
+    {
+      printInHelper(parent.getLeft())
+      system.out.println(parent.getValue());
+      printInHelper(parent.getRight()
+    }
+  }
 }
