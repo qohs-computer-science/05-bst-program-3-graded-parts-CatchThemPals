ce62f5dc38558d065c553543f6ad42dc2346464e
CatchThemPals <sx86851@gmail.com>
2025-02-28 10:05:37 -0500

    Update BST.java(google docs)

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -1,7 +1,9 @@
 import java.lang.Comparable;
 
+
 public class BST implements BSTInterface
 {
+
 
   private TreeNode root;
   public BST()
@@ -33,56 +35,52 @@ public class BST implements BSTInterface
   }
   public boolean delete(Comparable old)
   {
-    	if(root != null)
-	{
-	if(old.compareTo(root.getValue()) == 0)
+      if(root != null)
+  {
+  if(old.compareTo(root.getValue()) == 0)
   {
     // 4 cases:
     // root no kids
     if(root.getLeft() == null) // Checks for left is null, then see if right is null, then if first is false and second is true, then make root right, else make root null and return true
-    { 
-      if(root.getRight() == null)
+    {
+      if(root.getRight() == null) // no kids
       {
         root = null;
         return true;
       }
-      else
+      else // right kid only
       {
         root = root.getRight();
         return true;
-      } 
+      }
     }
-    else if(root.getRight() == null)// Checks for right is null, then see if left is null, then if first is false and second is true, then make root left, else make root null and return true
-    { 
+    else if(root.getLeft() != null)// Checks for right is null, then see if left is null, then if first is false and second is true, then make root left, else make root null and return true
     {
-      if(root.getLeft() == null)
+      if(root.getRight() == null) // left kid only
       {
-        root = null;
+        root = root.getLeft();
         return true;
       }
-      else
+      else // two kids
       {
         root = root.getLeft();
         return true;
       }  
     }
-    else
-    {
-      root.set 
-    }
+
 
     // root left kid only - root = left
     // root right kid only = root = right
     // root both kids -- replaced with left kid, and the right kid becomes the right-most kid of the new root -- while loop
   }
-	// delete this one - 4 cases - left only, right only, 2 kids, no kids
-		else if (old.compareTo(root.getValue() < 0)
-	deleteHelper(old, root, root.getLeft())
-	 //          (value, parent, child)
-	else
-	deleteHelper(old, root, root.right())
-	 //          (value, parent, child)
-	}
+  // delete this one - 4 cases - left only, right only, 2 kids, no kids
+    else if (old.compareTo(root.getValue() < 0)
+  deleteHelper(old, root, root.getLeft())
+   //          (value, parent, child)
+  else
+  deleteHelper(old, root, root.right())
+   //          (value, parent, child)
+  }
   else
   {
     return false;
@@ -90,9 +88,9 @@ public class BST implements BSTInterface
   }
 private boolean deleteHelper(Object old, TN parent, TN child)
  // determine if child is the thing to delete
-	if(child != null)
-	{
-	if(old.compareTo(child.getValue) == 0)
+  if(child != null)
+  {
+  if(old.compareTo(child.getValue) == 0)
   {
     if(child.getRight() == null)
     {
@@ -116,96 +114,99 @@ private boolean deleteHelper(Object old, TN parent, TN child)
       else
       {
 
+
       }
     }
     else
     {
-      
+     
     }
   }
-	// delete this one - 8 cases
+  // delete this one - 8 cases
 Child is a LC of parent  - left only, right only, 2 kids, no kids
-	// check no kids would be 
+  // check no kids would be
 if(child.getLeft() == null && child.getRight() == null)
-	// parent.setLeft(null);
-	//parent.setLeft(child.getLeft());
-	//parent.setLeft(child.getRight());
-	//parent.setLeft
+  // parent.setLeft(null);
+  //parent.setLeft(child.getLeft());
+  //parent.setLeft(child.getRight());
+  //parent.setLeft
 Child is a RC of parent - left only, right only, 2 kids, no kids
-	// check no kids would be 
+  // check no kids would be
 if(child.getLeft() == null && child.getRight() == null)
-	// parent.setRight(null);
-	// parent.setRight(child.getLeft());
-	// parent.setRight(child.getRight());
-	
-	else if object is < root
+  // parent.setRight(null);
+  // parent.setRight(child.getLeft());
+  // parent.setRight(child.getRight());
+ 
+  else if object is < root
 deleteHelper(old, root, root.getLeft())
-	 //          (value, parent, child)
-		Else
+   //          (value, parent, child)
+    Else
 deleteHelper(old, root, root.right())
-	 //          (value, parent, child)
-	}
-	else 
-		Return false; // not in the tree to delete
-	if subroot =  value
-{	
-	if(subroot.getLeft(subroot) != null & subroot.getRight(subroot) != null)
-	{
-	parent.setLeft(subroot.getleft(subroot))
-	
-	}
-	Else if(subroot.getLeft
+   //          (value, parent, child)
+  }
+  else
+    Return false; // not in the tree to delete
+  if subroot =  value
+{
+  if(subroot.getLeft(subroot) != null & subroot.getRight(subroot) != null)
+  {
+  parent.setLeft(subroot.getleft(subroot))
+ 
+  }
+  Else if(subroot.getLeft
 }
 Else if subroot value is less than or equal to object
 {
-	printInAdder(subroot.getLeft())
+  printInAdder(subroot.getLeft())
 }
 Else if subroot value is greater than object
 {
-	printInAdder(subroot.getRight())
-	Else
-	Return false
+  printInAdder(subroot.getRight())
+  Else
+  Return false
 
 
-  
+
+
+ 
   public void add(Comparable newVal)
   {
-  	if(root != null)
-	  {
-		  if(newVal.compareTo(root.getValue()) <= 0)
+    if(root != null)
+    {
+      if(newVal.compareTo(root.getValue()) <= 0)
       {
         addHelper(newVal, root.getLeft(),root);
       }
-		  else
+      else
         {
-		    addHelper(newVal, root.getRight(),root);
+        addHelper(newVal, root.getRight(),root);
         }
     }
     else
     {
-      
+     
       root = new TreeNode(newVal,null,null);
-      
+     
     }
   }
 private void addHelper(Comparable newVal,TreeNode subroot,TreeNode parent){
 if (subroot != null){
-	if(newVal.compareTo(subroot.getValue()) <= 0)
-	{
-	addHelper(newVal, subroot.getLeft(),subroot);
-	}
-	else
-	{
-	addHelper(newVal, subroot.getRight(),subroot);
-	}
-		
+  if(newVal.compareTo(subroot.getValue()) <= 0)
+  {
+  addHelper(newVal, subroot.getLeft(),subroot);
+  }
+  else
+  {
+  addHelper(newVal, subroot.getRight(),subroot);
+  }
+   
   }
 else
   {
   if(newVal.compareTo(parent.getValue()) <= 0)
   {
   parent.setLeft(new TreeNode(newVal,null,null));
-  
+ 
   }
   else
   {
@@ -213,6 +214,7 @@ else
   }
   }
 }
+
 
   public void printPreOrder()
   { // Prints it in preOrder root,left,right
@@ -223,6 +225,7 @@ else
       printPreHelper(root.getRight());
     }
   }
+
 
 private void printPreHelper(TreeNode subroot)
   {
@@ -243,8 +246,9 @@ public void printInOrder()
     }
   }
 
+
 private void printInHelper(TreeNode subroot)
-  { 
+  {
     if(subroot != null)
     {
       printInHelper(subroot.getLeft());
@@ -252,6 +256,7 @@ private void printInHelper(TreeNode subroot)
       printInHelper(subroot.getRight());
     }
   }
+
 
 public void printPostOrder()
   { // Print in Post order left,right,root
@@ -263,6 +268,7 @@ public void printPostOrder()
     }
   }
 
+
 private void printPostHelper(TreeNode subroot)
   {
     if(subroot != null)
@@ -273,3 +279,5 @@ private void printPostHelper(TreeNode subroot)
     }
   }
 }
+
+
